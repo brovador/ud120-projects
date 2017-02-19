03e79b58d1cb1c2d52707f8976a2e1c41f7b0b19
Jesús Bartús Sancho <jesus.bartus@gmail.com>
2017-02-19 09:33:24 +0100

    validation exercises

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -14,6 +14,8 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.model_selection import train_test_split
+from sklearn import tree
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -25,8 +27,8 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
-
-
-### it's all yours from here forward!  
-
-
+# Split data into training/test
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)

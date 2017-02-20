66111a6cc6626813ffa4a6b730f4ba756fb1d6c1
Jesús Bartús Sancho <jesus.bartus@gmail.com>
2017-02-20 20:37:59 +0100

    Evaluation metrics

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -15,6 +15,9 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.metrics import precision_score, recall_score, f1_score
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -24,8 +27,18 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+# Split data into training/test
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
 
+pred = clf.predict(features_test)
 
-### your code goes here 
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+print 'precision: ', precision_score(true_labels, predictions)
+print 'recall: ', recall_score(true_labels, predictions)
+print 'f1_score: ', f1_score(true_labels, predictions)
 
 
